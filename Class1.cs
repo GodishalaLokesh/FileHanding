7beda9ce1596828bd7078644a1ce57cd358cc15c
Lokesh Godishala <lokeshgodishala9@gmail.com>
2026-07-30 22:39:27 +0530

    Added Class1 file

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandling
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello from >net to GitHub");
+        }
+    }
+}
